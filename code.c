0969e7e2e461a5406af3b04288d8d460babe05c5
Edmanito <168591861+Edmanito@users.noreply.github.com>
2025-04-06 21:29:53 +0200

    blalaa

--- code.c
+++ code.c
@@ -1,5 +1,5 @@
-// Pour compiler        gcc code.c -o code.x -lSDL2
-// Pour lancer          ./code.x
+// Pour compiler        gcc code.c -o run -lSDL2
+// Pour lancer          ./run
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
@@ -10,12 +10,8 @@
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* fenetre = SDL_CreateWindow("Ma fenêtre SDL2",
-        SDL_WINDOWPOS_CENTERED, 
-		SDL_WINDOWPOS_CENTERED,
-		500,
-		500,
-		0);
+    SDL_Window* fenetre = SDL_CreateWindow("Ma fenêtre",SDL_WINDOWPOS_CENTERED
+                                                            ,SDL_WINDOWPOS_CENTERED,500,500,0);
 
     SDL_Event event;
     int running = 1;
