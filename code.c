69fd02868473c87a2a2c6d4a3ac962a5f82e597f
Edmanito <168591861+Edmanito@users.noreply.github.com>
2025-04-07 10:31:08 +0200

    delete code.x

--- code.c
+++ code.c
@@ -5,29 +5,33 @@
 #include <stdlib.h>
 #include <time.h>
 
-//printf("yo");
 
 
 
 int main(int argc, char* argv[]) {
-    SDL_Init(SDL_INIT_VIDEO);
-
-    SDL_Window* fenetre = SDL_CreateWindow("Ma fenêtre",SDL_WINDOWPOS_CENTERED
-                                                            ,SDL_WINDOWPOS_CENTERED,500,500,0);
+    SDL_Init(SDL_INIT_VIDEO); //Window Init
+    if( SDL_Init(SDL_INIT_VIDEO)!=0){//
+        SDL_Log("SDL_Error %s",SDL_GetError());
+        return 1;
+    }
+  
+    SDL_Window* window = SDL_CreateWindow("Window",SDL_WINDOWPOS_CENTERED
+                                                  ,SDL_WINDOWPOS_CENTERED
+                                                  ,500,500,0); //Window creation
+    
 
     SDL_Event event;
-    int running = 1;
-
-    while (running) {
+    int window_loop = 1;
+    while(window_loop) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
-                running = 0;
+                window_loop = 0;
             }
         } //fe
         SDL_Delay(16); // pause = + fluide
     }
 
-    SDL_DestroyWindow(fenetre);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
